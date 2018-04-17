5e1f91464496a72f28176637ea24896808b61bf5
giancarlocarraro <gcarraro@gmail.com>
2018-04-16 21:13:54 -0300

    Upload project to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule, MatCardModule, MatIconModule  } from '@angular/material';
+import { ImgCardComponent } from './img-card/img-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
